fca959c857b71d24e5377a6e87ff6db72b4c9a19
Andreas Gohr <andi@splitbrain.org>
2021-11-23 15:43:50 +0100

    fix non-defined attribute error

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 jQuery(function (){
     jQuery('a.wikilink1, a.wikilink2').each(function (idx, elem){
 
-        if(elem[title] && JSINFO.plugins.lms.seen.includes(elem.title)) {
+        if(elem['title'] && JSINFO.plugins.lms.seen.includes(elem.title)) {
             jQuery(elem).addClass('lms-seen');
         }
 
