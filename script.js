7c9deb5358bb0e78a378db88456c500f9833fcec
Andreas Gohr <andi@splitbrain.org>
2021-11-30 10:19:03 +0100

    guard against undefined data
    when not logged in or without completed lessons, no data is set in
    JSINFO so our script should abort early

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 jQuery(function (){
+    if(!JSINFO['plugins']) return;
+    if(!JSINFO.plugins['lms']) return;
+
     jQuery('a.wikilink1, a.wikilink2').each(function (idx, elem){
 
         if(elem['title'] && JSINFO.plugins.lms.seen.includes(elem.title)) {
