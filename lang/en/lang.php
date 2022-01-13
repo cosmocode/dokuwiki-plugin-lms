4e87b6be9d057710aae37d690799527ec14b7651
Anna Dabrowska <dabrowska@cosmocode.de>
2022-01-13 14:55:44 +0100

    Adjust localization

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -10,10 +10,10 @@ $lang['menu'] = 'Lesson Completions';
 $lang['username'] = 'Lookup user:';
 $lang['status'] = 'Completion Status for %s';
 
-$lang['seen'] = 'Toggle lesson as seen';
-$lang['check'] = 'Toggle lesson complete, move to next lesson';
-$lang['unseen'] = 'Toggle lesson as unseen';
-$lang['next'] = 'Next lesson';
-$lang['prev'] = 'Previous lesson';
+$lang['seen'] = 'Toggle card studied';
+$lang['check'] = 'Toggle card studied, move to next one';
+$lang['unseen'] = 'Toggle card to study';
+$lang['next'] = 'Next card';
+$lang['prev'] = 'Previous card';
 
 
